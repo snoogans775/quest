9daae46186b83f2dafc3ec9217935e36e4c06c28
snoogans775 <snoogans775@gmail.com>
2017-01-26 01:10:56 -0800

    removed previous logins for Quest databases

--- includes/db_connection.php
+++ includes/db_connection.php
@@ -1,8 +1,8 @@
 <?php
 define("DB_SERVER", "localhost");
-define("DB_USER", "rvgsym5_admin");
-define("DB_PASS", "Koj1is#1");
-define("DB_NAME", "rvgsym5_quest");
+//define("DB_USER", "");
+//define("DB_PASS", "");
+//define("DB_NAME", "");
 	//creates a database connection
 	$connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
 	// tests connection for errors
