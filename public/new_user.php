b3db8880a59b41d5164bbbb12db7afcace59e49b
Kevin Fredericks <snoogans775@gmail.com>
2020-08-21 13:36:40 -0700

    refactored validation to Database, Validator, and Encryptor

--- public/new_user.php
+++ public/new_user.php
@@ -1,98 +1,42 @@
-<?php require_once("../includes/session.php"); ?>
-<?php include("../includes/db_connection.php"); ?>
-<?php require_once("../includes/functions.php"); ?>
-<?php require_once("../includes/validation_functions.php"); ?>
-<?php $layout_context = "public"; ?>
-<?php find_selected_page(); ?>
+<?php require_once("../includes/Database.php"); ?>
+<html>
+<head>
+	<meta charset="utf-8"></meta>
+	<title>The Quest</title>
+	<link rel="stylesheet" href="css/style.css" type="text/css" />
+	<link rel="stylesheet" type="text/css"
+	      href="https://fonts.googleapis.com/css?family=IM+Fell+Great+Primer|Nunito|Averia+Sans+Libre" />
+	<link rel="shortcut icon" href="../favicon.ico" type="image/x-icon" />
+</head>
+<body>
+<?php 
 
-<?php /*
+$db = new Database();
+$db->setDefaultConnection();
+
 if (isset($_POST["submit"])) {
-	//validations
-	$required_fields = array("username", "password");
-	validate_presences($required_fields);
-	
-	$fields_with_max_lengths = array("username" => 30);
-	validate_max_lengths($fields_with_max_lengths);
-
-	validate_email("email");
-
-	match_passwords("password", "password_confirm");
-	
-	//EMERGENCY VALIDATION QUESTION, FIX WITH A CAPTCHA OR A fUNCTION
-	
-	$answer = strtolower($_POST["validate"]);
-	if ($answer !== "doki") {
-		$errors[] = "Please answer the security qustion correctly";
-	}
-	
-	if (empty($errors)) {
-		$username = mysql_prep($_POST["username"]);
-		$email = mysql_prep($_POST["email"]);
-		$hashed_password = password_encrypt($_POST["password"]);
-
-		//performs database query
-		$query  = "INSERT INTO users ("; 
-		$query .= " username, hashed_password, points, email, date_joined";
-		$query .= ") VALUES (";
-		$query .= " '{$username}', '{$hashed_password}', 400, '{$email}', NOW() ";
-		$query .= ")";
-		$result = mysqli_query($connection, $query); 
-		//$result will not be a typical variable. It will be a resource.
-	
-		if ($result && mysqli_affected_rows($connection) >= 0) {
-			//Success
-			$_SESSION["message"] = "Account Created.";
-			redirect_to("new_forum_user.php?user=". $_POST["username"]. "&pass=". $_POST["password"]);
-		} else {
-			//Failure
-			$_SESSION["message"] = "Registration failed.";
-			redirect_to("new_user.php");
-		}
-		
-	} else {
-	// This is probably a GET request
-	// END OF if (isset($_POST["submit"]))	
-	}
+	$db->addUser($_POST);
+	print_r( $db->errors) ;
 }
 
-*/?>
-<?php include("../includes/header.php"); ?>
-<div class="page">
-	<?php 
-		if (!isset($_SESSION["username"])) { ?>
-			<h1>Sorry, The Quest is in Winter hibernation. Check the <a href="/forum.php">forums if you have any questions. Thanks!</h1>
-			<div>
-				<?php echo form_errors($errors); ?>
-				<?php echo $_SESSION["message"]; $_SESSION["message"] = null; ?>
-			</div>
-			<!-- DISCONTINuED/ CHECK LINE 98 for CLOSING COMMENT
-			<form action="new_user.php" method="POST">
-				<table>
-					<tr>
-				<td>Username: </td><td><input type="text" name="username" placeholder="ex. vidjyagamesBilly" /></td>
-					</tr>
-					<tr>
-				<td>Email: </td><td><input type="text" name="email" placeholder="ex. praise@vidjya.games"/></td>
-					</tr>
-					<tr>
-				<td>Password: </td><td><input type="password" name="password" placeholder="" /></td>
-					</tr>
-					<tr>
-				<td>Confirm Password: </td><td><input type="password" name="password_confirm" placeholder="" /></td>
-					</tr>
-					<tr>
-						<td>Fill in The Blank: Doki _____ Panic </td><td><input type="validate" name="validate" value="" /></td>
-					</tr>
-				</table>
-				
-				<input type="submit" name="submit" value="Register" />
-			</form>
-				-->
-		<?php } else {
-			echo "<p>Please log out before creating a new user. And please don't make more than one user account. It is poor form. Very poor.</p>";
-		} ?>
-	
-</div>
-<?php include("../includes/footer.php"); ?>
+?>
+	<form action="new_user.php" method="POST">
+		<table>
+			<tr>
+		<td>Username: </td><td><input type="text" name="username" placeholder="ex. vidjyagamesBilly" /></td>
+			</tr>
+			<tr>
+		<td>Email: </td><td><input type="text" name="email" placeholder="ex. praise@vidjya.games"/></td>
+			</tr>
+			<tr>
+		<td>Password: </td><td><input type="password" name="password" placeholder="" /></td>
+			</tr>
+			<tr>
+		<td>Confirm Password: </td><td><input type="password" name="password_confirm" placeholder="" /></td>
+			</tr>
+		</table>
+		
+		<input type="submit" name="submit" value="Register" />
+	</form>
 </body>
 </html>
