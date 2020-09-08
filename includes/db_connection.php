6135396beeb3b0177cc6fbd49bdd811f16a4fac5
Kevin Fredericks <snoogans775@gmail.com>
2020-09-08 00:10:30 -0700

    new db credential

--- includes/db_connection.php
+++ includes/db_connection.php
@@ -1,7 +1,7 @@
 <?php
 define("DB_SERVER", "localhost");
 define("DB_USER", "quest_admin");
-define("DB_PASS", "MIwatais#4");
+define("DB_PASS", "Koj1is#1");
 define("DB_NAME", "quest");
 	//creates a database connection
 	$connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
