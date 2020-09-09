e79e84e9fe8561739a4e566c91f46823d197f064
Kevin Fredericks <snoogans775@gmail.com>
2020-09-09 11:27:17 -0700

    add env support

--- includes/db_connection.php
+++ includes/db_connection.php
@@ -1,8 +1,13 @@
 <?php
-define("DB_SERVER", "localhost");
-define("DB_USER", "snoogans775");
-define("DB_PASS", "+Pe5Qr6%unC-i");
-define("DB_NAME", "quest");
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . "/../");
+$dotenv->load();
+
+define("DB_SERVER", $_ENV['DEVSERVER']);
+define("DB_USER", $_ENV['DBUSER']);
+define("DB_PASS", $_ENV['PASSWORD']);
+define("DB_NAME", $_ENV['DBNAME']);
 	//creates a database connection
 	$connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
 	// tests connection for errors
