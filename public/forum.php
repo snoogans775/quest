fdf134779491cbf56caadd2940d231dee18ea5ed
Kevin Fredericks <snoogans775@gmail.com>
2020-10-06 12:10:54 -0700

    added sorry message for deprecation

--- public/forum.php
+++ public/forum.php
@@ -1,1 +1,2 @@
-<?php redirect_to("http://www.rvgsymphony.com/quest/forum"); ?>
+<h2>Sorry, the forum has been deprecated.</h2>
+<p>Please contact Kevin at <a mailto="kfrednv@gmail.com">kfrednv@gmail.com</a> if you need access to older forum posts</p>
